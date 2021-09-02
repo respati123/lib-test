99b64e3d8a8b804e212bedb7aee6c02d1d3c3a61
cst_dev128 <cst_dev128@unitedtractors.com>
2021-09-02 12:20:11 +0700

    first

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external"
+import typescript from '@rollup/plugin-typescript'
+import postcss from "rollup-plugin-postcss"
+
+import packageJson from "./package.json"
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    input: "./src/index.ts",
+    output: [
+        {
+            file: packageJson.main,
+            format: "cjs",
+            sourcemap: true
+        },
+        {
+            file: packageJson.module,
+            format: "esm",
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        PeerDepsExternalPlugin(),
+        resolve(),
+        commonjs(),
+        typescript(),
+        postcss()
+    ]   
+}
